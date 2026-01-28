205b298dbd1040c2ec846f143aea5855dcefba6b
Marcel Sauder <marcel@sauder.ch>
2026-01-28 21:13:33 +0100

    Sensor layer stable: SHT3x + DS18B20 combined, modular

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,60 @@
 #include <Arduino.h>
 #include <Wire.h>
+
 #include "sht3x.h"
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 4   // GPIO gemss Doku
+
+// DS18B20
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature ds18b20(&oneWire);
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  Serial.println();
-  Serial.println("=== SHT3X ROBUST MODULE TEST ===");
-
+  // I2C zentral
   Wire.begin();
+  delay(200);
 
-  if (!sht3x_init()) {
-    Serial.println("ERROR: SHT3X not detected");
-    while (true) {
-      delay(1000);
-    }
+  // SHT3x
+  if (sht3x_init()) {
+    Serial.println("SHT3X OK");
+  } else {
+    Serial.println("SHT3X INIT FAILED");
   }
 
-  Serial.println("SHT3X detected");
+  // DS18B20
+  ds18b20.begin();
+  Serial.println("DS18B20 OK");
+
+  Serial.println("=== SENSOR COMBINED TEST ===");
 }
 
 void loop() {
-  SHT3xData data = sht3x_read();
+  // --- SHT3x ---
+  SHT3xData sht = sht3x_read();
+  Serial.print("SHT3X  T=");
+  Serial.print(sht.temperature, 2);
+  Serial.print(" C  H=");
+  Serial.print(sht.humidity, 2);
+  Serial.println(" %");
 
-  if (data.status == SHT3xStatus::OK) {
-    Serial.print("Temperature: ");
-    Serial.print(data.temperature, 2);
+  // --- DS18B20 ---
+  ds18b20.requestTemperatures();
+  float t_ds = ds18b20.getTempCByIndex(0);
+
+  if (t_ds != DEVICE_DISCONNECTED_C) {
+    Serial.print("DS18B20 T=");
+    Serial.print(t_ds, 2);
     Serial.println(" C");
-
-    Serial.print("Humidity: ");
-    Serial.print(data.humidity, 2);
-    Serial.println(" %RH");
   } else {
-    Serial.print("SHT3X ERROR: ");
-    Serial.println(sht3x_status_to_string(data.status));
+    Serial.println("DS18B20 READ ERROR");
   }
 
   Serial.println("---");
-  delay(5000);
+  delay(2000);
 }
